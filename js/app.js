60c5b063f32b5c9577d750bb99ded4af99f013ec
sanuj saxena <sanujs97@gmail.com>
2019-02-24 21:35:18 +0530

    why us integrated

---
+++ js/app.js
@@ -0,0 +1,16 @@
+var app = angular.module('sih2019', ['ngRoute']);
+
+app.config(['$routeProvider', function ($routeProvider, $locationProvider) {
+
+   // $locationProvider.hashPrefix('!');
+
+    $routeProvider.
+        when('/',{
+            templateUrl: '/home.html',
+            controller: 'homeController'
+        })
+        .when('/why-us', {
+            templateUrl: '/why-us.html',
+            controller: 'whyUsController'
+        });
+}]);
