39e5c5bf5f893fa1696ec916c905bccc8e0be19d
himanshukhosla123 <himanshukhosla123@gmail.com>
2019-03-02 00:59:54 +0530

    architectutre setup done

--- js/app.js
+++ js/app.js
@@ -12,5 +12,29 @@ app.config(['$routeProvider', function ($routeProvider, $locationProvider) {
         .when('/why-us', {
             templateUrl: './why-us.html',
             controller: 'whyUsController'
+        })
+        .when('/contact-us', {
+            templateUrl: './contact-us.html',
+            // controller: 'conUsController'
+        })
+        .when('/other-apps', {
+            templateUrl: './other-apps.html',
+            // controller: 'whyUsController'
+        })
+        .when('/other-services', {
+            templateUrl: './other-services.html',
+            // controller: 'whyUsController'
+        })
+        .when('/sync-ppt', {
+            templateUrl: './sync-ppt.html',
+            // controller: 'whyUsController'
+        })
+        .when('/testimonials', {
+            templateUrl: './testimonials.html',
+            // controller: 'whyUsController'
+        })
+        .when('/bonus', {
+            templateUrl: './bonus.html',
+            // controller: 'whyUsController'
         });
 }]);
